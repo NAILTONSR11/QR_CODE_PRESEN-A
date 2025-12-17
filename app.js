4cd20d6402d90c9593082250a21b77ce7f512a5a
Nailton <nailtonsr01@gmail.com>
2025-12-17 08:59:23 -0300

    ajuste nas rotas amigaveis

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(express.json());
 // ==================
 // Arquivos estáticos
 // ==================
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "pages")));
 
 
 app.use("/api/users", userRoutes);
